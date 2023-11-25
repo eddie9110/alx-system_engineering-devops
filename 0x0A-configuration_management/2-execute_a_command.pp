1ef1d66578d34ff087d316075cb8f7a542cbe1ff
eddie9110 <edwinmaregwa@gmail.com>
2023-11-25 04:33:30 -0800

    all tasks

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,4 @@
+# Using Puppet, create a manifest that kills a process named killmenow
+exec {'kill killmenow':
+  command => '/usr/bin/pkill killmenow',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+This project was about configuration management using Puppet
