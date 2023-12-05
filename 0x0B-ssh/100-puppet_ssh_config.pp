30a4f85828f06c1c7366923632bcbc06462038f3
eddie9110 <edwinmaregwa@gmail.com>
2023-12-05 05:19:14 -0800

    task 4 added

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# automating config files using puppet
 file { 'etc/ssh/ssh_config' :
 	ensure => present,
 content =>" 
 	host*
 	IdentityFile ~/.ssh/school
-	PasswordAuthentication no"
+	PasswordAuthentication no",
 }
